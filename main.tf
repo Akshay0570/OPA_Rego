86e527b494944f3ed471c36bf8aafc540b827e44
akshay0570 <100024873+Akshay0570@users.noreply.github.com>
2023-06-21 15:35:56 +0530

    Create main.tf
    created basic tf file for ec2,vpc and subnets creation

---
+++ main.tf
@@ -0,0 +1,69 @@
+provider "aws" {
+    access_key  = "AKIATILXC7K3L72MWGPU"
+    secret_key  = "cwSvKMXz+gBLVHDgSpNbSv054nRiwnmnuGSgF4GB"
+    region      = "ap-south-1"
+}
+# lets try to create ec2-instance
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+}
+
+
+# lets try to define the resource for the vpc
+resource "aws_vpc" "myvpc" {
+    cidr_block = "192.168.0.0/16"
+
+    tags = {
+      "Name" = "from-tf"
+    }
+
+}
+
+# lets create web1 subnet
+resource "aws_subnet" "web1" {
+    vpc_id              = aws_vpc.myvpc.id
+    cidr_block          = "192.168.0.0/24"
+    availability_zone   = "ap-south-1a"
+
+    tags                = {
+      "Name"            = "web1-tf"
+    }
+
+}
+
+# lets create web2 subnet
+resource "aws_subnet" "web2" {
+    vpc_id              = aws_vpc.myvpc.id
+    cidr_block          = "192.168.1.0/24"
+    availability_zone   = "ap-south-1b"
+
+    tags                = {
+      "Name"            = "web2-tf"
+    }
+
+}
+
+# lets create db1 subnet
+resource "aws_subnet" "db1" {
+    vpc_id              = aws_vpc.myvpc.id
+    cidr_block          = "192.168.2.0/24"
+    availability_zone   = "ap-south-1a"
+
+    tags                = {
+      "Name"            = "db1-tf"
+    }
+
+}
+
+# lets create db2 subnet
+resource "aws_subnet" "db2" {
+    vpc_id              = aws_vpc.myvpc.id
+    cidr_block          = "192.168.3.0/24"
+    availability_zone   = "ap-south-1b"
+
+    tags                = {
+      "Name"            = "db2-tf"
+    }
+
+}
